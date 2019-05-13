712a88c0328b187e3b5e96b82750a760ea92d3f8
t0byn <tobyn112@outlook.com>
2019-05-13 22:15:37 +0800

    lab3 exercise 4 done

--- lab3/kern/trap.h
+++ lab3/kern/trap.h
@@ -20,4 +20,24 @@ void print_trapframe(struct Trapframe *tf);
 void page_fault_handler(struct Trapframe *);
 void backtrace(struct Trapframe *);
 
+/* interrupt handler */
+void intr_handler_0();
+void intr_handler_1();
+void intr_handler_2();
+void intr_handler_3();
+void intr_handler_4();
+void intr_handler_5();
+void intr_handler_6();
+void intr_handler_7();
+void intr_handler_8();
+void intr_handler_10();
+void intr_handler_11();
+void intr_handler_12();
+void intr_handler_13();
+void intr_handler_14();
+void intr_handler_16();
+void intr_handler_17();
+void intr_handler_18();
+void intr_handler_19();
+
 #endif /* JOS_KERN_TRAP_H */

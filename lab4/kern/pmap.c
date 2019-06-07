5300a031c50dced2e02673bd5126efed9ce881cc
t0byn <tobyn112@outlook.com>
2019-06-07 10:08:34 +0800

    lab4 exercise 3 done

--- lab4/kern/pmap.c
+++ lab4/kern/pmap.c
@@ -287,6 +287,13 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
+	// My code:
+	for (size_t i = 0; i < NCPU; i++) {
+		uintptr_t kstacktop_i = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
+
+		boot_map_region(kern_pgdir, kstacktop_i - KSTKSIZE, 
+				KSTKSIZE, PADDR(percpu_kstacks[i]), PTE_W);
+	}
 
 }
 

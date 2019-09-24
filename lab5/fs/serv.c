528f533a9e40fc85a20685a2de3d33b87e5cb577
t0byn <tobyn112@outlook.com>
2019-09-24 18:12:24 +0800

    lab5 exercise 6 done

--- lab5/fs/serv.c
+++ lab5/fs/serv.c
@@ -246,7 +246,26 @@ serve_write(envid_t envid, struct Fsreq_write *req)
 		cprintf("serve_write %08x %08x %08x\n", envid, req->req_fileid, req->req_n);
 
 	// LAB 5: Your code here.
-	panic("serve_write not implemented");
+	// panic("serve_write not implemented");
+	// My code:
+	struct OpenFile *o;
+	int r;
+
+	// First, use openfile_lookup to find the relevant open file.
+	if ((r = openfile_lookup(envid, req->req_fileid, &o)) < 0)
+		return r;
+
+	// Second, call the relevant file system function (from fs/fs.c).
+	if ((r = file_write(o->o_file, 
+					(void *) req->req_buf, 
+					req->req_n, 
+					o->o_fd->fd_offset)) < 0)
+		return r;
+
+	// update the seek position
+	o->o_fd->fd_offset += r;
+
+	return r;
 }
 
 // Stat ipc->stat.req_fileid.  Return the file's struct Stat to the
--- lab5/lib/file.c
+++ lab5/lib/file.c
@@ -141,7 +141,17 @@ devfile_write(struct Fd *fd, const void *buf, size_t n)
 	// remember that write is always allowed to write *fewer*
 	// bytes than requested.
 	// LAB 5: Your code here
-	panic("devfile_write not implemented");
+	// panic("devfile_write not implemented");
+	// My code:
+	fsipcbuf.write.req_fileid = fd->fd_file.id;
+	fsipcbuf.write.req_n = n;
+	assert(n <= PGSIZE - (sizeof(int) + sizeof(size_t)));
+	memcpy(fsipcbuf.write.req_buf, buf, n);
+
+	int r = fsipc(FSREQ_WRITE, NULL);
+	assert(r <= n);
+
+	return r;
 }
 
 static int
